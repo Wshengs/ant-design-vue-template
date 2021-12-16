dc3a59f141db7828a8976e18057fa6a40ebfd8ef
wu sheng <sheng.wu@beyonca.com>
2021-12-16 11:31:13 +0800

    init: 初始化

---
+++ src/pages/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+    <Title>Hello</Title>
+    Feboot
+</template>
+
+<script>
+export default {
+    name: "HelloWorld"
+}
+</script>
+
+<style scoped>
+ h5 {color:#555;font-size: 16px;}
+</style>
